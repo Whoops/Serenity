225b46897aed85bb12526ba9bf50f4c668a21bec
Walton Hoops <me@waltonhoops.com>
2013-04-11 16:32:17 -0600

    use libtagc instead of taglib due to crashes

--- src/Import.hs
+++ src/Import.hs
@@ -9,7 +9,8 @@ import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (fromJust)
 import Data.Acid
 import Data.Aeson
-import qualified Sound.TagLib as TagLib
+import Audio.TagLib.TagLib
+import Data.ByteString.Char8 (pack)
 
 processDirectory database path = do
   dirs >>= mapM_ (processDirectory database)
@@ -20,8 +21,8 @@ processDirectory database path = do
         dirs = contents >>= filterM doesDirectoryExist
         files = contents >>= filterM doesFileExist
   
-extractTag filename = do tagFile <- TagLib.open filename
-                         tag <- TagLib.tag $ fromJust tagFile
+extractTag filename = do tagFile <- tagFileOpen $ pack filename
+                         tag <- tagFileGetTag $ fromJust tagFile
                          return $ fromJust tag
 
 --processFile :: FilePath -> IO ()
@@ -33,12 +34,12 @@ processFile database path = when (takeExtension path == ".mp3") $ do
 
 --insertFile :: FilePath -> TagLib.Tag -> IO ()
 insertFile database file tag = do
-  artist <- TagLib.artist tag
-  album <- TagLib.album tag
-  title <- TagLib.title tag
-  genre <- TagLib.genre tag
-  comment <- TagLib.comment tag
-  track <- TagLib.track tag
-  year <- TagLib.year tag
-  update database (AddTrack file artist album title genre comment track year)
+  artist <- tagGetArtist tag
+  album <- tagGetAlbum tag
+  title <- tagGetTitle tag
+  genre <- tagGetGenre tag
+  comment <- tagGetComment tag
+  track <- tagGetTrack tag
+  year <- tagGetYear tag
+  update database (AddTrack file artist album title genre comment (toInteger track) (toInteger year))
   return ()
