2e53e768c4a5da76168e3c1eb849c1e791131d8f
Walton Hoops <me@waltonhoops.com>
2013-04-29 20:05:58 -0600

    use angular instead of knockout

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,57 @@
+function TrackController($scope, $rootScope, $http)
+{
+    $scope.tracks = [];
+    $http.get('/tracks').success(function (data) {
+        $scope.tracks = data;
+    });
+
+    $scope.queue = function (track) {
+        $rootScope.$emit('enqueue', track);
+    }
+}
+
+function PlayingController($scope, $rootScope) {
+    $scope.tracks = [];
+    $scope.current = 0;
+    $scope.playing = function () {
+        if ($scope.current < $scope.tracks.length)
+            return $scope.tracks[$scope.current];
+        else
+            return null;
+    }
+
+    $rootScope.$on('enqueue', function (event, track) {
+        track.playing = function () {
+            if (track === $scope.playing()) {
+                return ["playing"];
+            }
+        }
+        $scope.tracks.push(track);
+        event.stopPropagation();
+    });
+
+    $scope.$watch('playing()', function (track) {
+        if(track) {
+            $("#player").jPlayer("setMedia", { mp3:  "/tracks/" + track.id });
+            $("#player").jPlayer("play");
+        }
+    });
+
+    $("#player").bind($.jPlayer.event.ended, function () {
+        $scope.current += 1;
+        $scope.$digest();
+    });
+}
+
+$(function () {
+    $("#player").jPlayer({ swfPath: "js/jquery/" });
+});
+
+/*function ContentViewModel () {
+    var self = this;
+    self.tracks = new TrackList ();
+}
+
 function TrackList () {
     var self = this;
     self.tracks = ko.observableArray();
@@ -10,18 +64,15 @@ function Playlist () {
     var self = this;
     self.tracks = ko.observableArray();
     self.current = ko.observable(0);
-    self.enqueue = function (track) {
-        self.tracks.push(track);
-    }
+
+    
     self.playing = ko.computed(function () {
         if (self.current() < self.tracks().length)
             return self.tracks()[self.current()];
         else
             return null;
     });
-    self.advance = function () {
-        self.current(self.current() + 1);
-    }
+    
     self.currentTitle = ko.computed( function () {
         var tr = self.playing()
         if (tr)
@@ -29,6 +80,18 @@ function Playlist () {
         else
             return "nothing";
     });
+    
+    
+    self.enqueue = function (track) {
+        track.selected = ko.computed(function () {
+            return track === self.playing();
+        });
+        self.tracks.push(track);
+    }
+    
+    self.advance = function () {
+        self.current(self.current() + 1);
+    }
 }
 var nowPlaying = new Playlist();
 
@@ -48,10 +111,13 @@ function playTrack () {
 }
 
 $(function () {
-    var allTracks = new TrackList();
+    //var allTracks = new TrackList();
+    var content = new ContentViewModel();
+    var allTracks = content.tracks;
+    
     $("#player").jPlayer({ swfPath: "js/jquery/" });
     
-    ko.applyBindings(allTracks, $("#content")[0]);
+    ko.applyBindings(content, $("#content")[0]);
     ko.applyBindings(nowPlaying, $("#playing")[0]);
 
     nowPlaying.playing.subscribe(playTrack());
@@ -60,59 +126,4 @@ $(function () {
     $.getJSON('/tracks').success(function (data) {
         allTracks.tracks(data);
     });
-});
-
-
-
-/*var templates = {}
-
-function registerTemplate (name) {
-    var reg = function (text) {
-        templates[name] = Mustache.compile(text);
-    }
-    return reg;
-}
-
-function renderContent (type) {
-    var template = templates[type];
-    var rend = function (data) {
-        var view = {}
-        view[type] = data;
-        $("#content").html(template(view));
-    }
-    return rend;
-}
-
-function registerTemplates () {
-    $.get("templates/artists.html").success(registerTemplate("artists"));
-    $.get("templates/tracks.html").success(registerTemplate("tracks"));
-}
-
-function displayArtists () {
-    $.getJSON("artists").success(renderContent("artists"));
-}
-
-function displayTracks(artist) {
-    $.getJSON("artists/" + artist + "/tracks").success(renderContent("tracks"));
-}
-
-function playTrack(track) {
-    $("#player").jPlayer("setMedia", { mp3:  "/tracks/" + track.attr("id") });
-    $("#player").jPlayer("play");
-}
-
-$(function () {
-    $("#player").jPlayer({ swfPath: "js/jquery/" });
-    registerTemplates();
-    $("#side-artists").click(function () {
-        displayArtists();
-    });
-    
-    $("#content").on("click", ".artist", function () {
-        displayTracks($(this).attr("id"));
-    });
-    $("#content").on("click", ".track", function () {
-        playTrack($(this));
-    });
-});
-*/
+}); */
